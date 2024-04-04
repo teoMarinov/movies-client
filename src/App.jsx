dd0b7d0c992524202e6818e50e8ed9afc83a8a4c
teoMarinov <teodor.b.marinov@gmail.com>
2024-04-04 21:50:00 +0300

    add reviews component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import api from './api/axiosConfig';
-import {useState, useEffect} from 'react';
-import {Layout} from './components/Layout';
-import {Routes, Route} from 'react-router-dom';
-import {Home} from './components/home/Home';
+import { useState, useEffect } from 'react';
+import { Layout } from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import { Home } from './components/home/Home';
 import Header from './components/header/Header';
 import Trailer from './components/trailer/Trailer';
-// import Reviews from './components/reviews/Reviews';
+import Reviews from './components/reviews/Reviews';
 import NotFound from './components/notFound/NotFound';
 
 function App() {
@@ -15,38 +15,34 @@ function App() {
   const [movie, setMovie] = useState();
   const [reviews, setReviews] = useState([]);
 
-  const getMovies = async () =>{
-    
-    try
-    {
+  const getMovies = async () => {
+
+    try {
 
       const response = await api.get("/api/v1/movies");
 
       setMovies(response.data);
 
-    } 
-    catch(err)
-    {
+    }
+    catch (err) {
       console.log(err);
     }
   }
 
   const getMovieData = async (movieId) => {
-     
-    try 
-    {
-        const response = await api.get(`/api/v1/movies/${movieId}`);
 
-        const singleMovie = response.data;
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
 
-        setMovie(singleMovie);
+      const singleMovie = response.data;
 
-        setReviews(singleMovie.reviews);
-        
+      setMovie(singleMovie);
 
-    } 
-    catch (error) 
-    {
+      setReviews(singleMovie.reviews);
+
+
+    }
+    catch (error) {
       console.error(error);
     }
 
@@ -54,18 +50,18 @@ function App() {
 
   useEffect(() => {
     getMovies();
-  },[])
+  }, [])
 
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route path="/" element={<Home movies={movies} />} ></Route>
-            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
-            {/* <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route> */}
-            <Route path="*" element = {<NotFound/>}></Route>
-          </Route>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />} />
+          <Route path="/Trailer/:ytTrailerId" element={<Trailer />} />
+          <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}/>
+          <Route path="*" element={<NotFound />} />
+        </Route>
       </Routes>
 
     </div>
